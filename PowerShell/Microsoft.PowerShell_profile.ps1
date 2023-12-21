2fe6aaec054e1e501a3b72db213b83d2091a0891
Nitesh Mishra <nitesh.mishra3@gmail.com>
2023-12-21 13:29:09 -0800

    Added Comments on PowerShell Profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,15 @@
+# Configure oh-my-posh with a specific theme
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\stelbent-compact.minimal.omp.json" | Invoke-Expression
 
-# Import Terminal Icons
+# Import the Terminal-Icons module for enhanced icons in the terminal
 Import-Module -Name Terminal-Icons
 
-# Quick shortcut to start notepad
+# Quick shortcut to start Notepad
 function n { notepad $args }
 
-# Simple function to start a new elevated process. If arguments are supplied then 
-# a single command is started with admin rights; if not then a new admin instance
-# of PowerShell is started.
+# Function to start a new elevated process
+# If arguments are supplied, a single command is started with admin rights
+# If no arguments, a new admin instance of PowerShell is started
 function admin {
     if ($args.Count -gt 0) {   
         $argList = "& '" + $args + "'"
@@ -18,129 +19,127 @@ function admin {
     }
 }
 
+# Shortcut to list files in the current directory
 function ll { Get-ChildItem -Path $pwd -File }
+
+# Shortcuts to navigate to specific directories and open them in VS Code
 function g { Set-Location $HOME\Documents\Github }
-function tt {
-    Set-Location "$HOME\Documents\tidytuesday"
-    code .
-}
-function da { 
-    Set-Location "$HOME\Documents\Data Analysis" 
-    code .
-}
-function bli {
-    Set-Location "$HOME\OneDrive - Scripps Research\Desktop Files\Data Analysis\BLI"
-    code .
-}
-function c {
-    code .
-}
+function tt { Set-Location "$HOME\Documents\tidytuesday"; code . }
+function da { Set-Location "$HOME\Documents\Data Analysis"; code . }
+function bli { Set-Location "$HOME\OneDrive - Scripps Research\Desktop Files\Data Analysis\BLI"; code . }
+
+# Shortcuts to open the current directory in VS Code
+function c { code . }
+
+# Git-related functions
 function gcom {
     git add .
     git commit -m "$args"
 }
+
 function lazyg {
     git add .
     git commit -m "$args"
     git push
 }
 
+# Function to create an empty file
 function touch($file) {
     "" | Out-File $file -Encoding ASCII
 }
 
+# Function to find the executable location of a command
 function which($name) {
     Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
-function rmrf($path)
-{
+# Recursive force removal of a directory
+function rmrf($path) {
     Remove-Item $path -Recurse -Force
 }
 
-function rm($path)
-{
+# Remove a file or directory
+function rm($path) {
     Remove-Item $path
 }
 
-function mv($path, $dest)
-{
+# Move a file or directory to a new destination
+function mv($path, $dest) {
     Move-Item $path $dest -Force -Recurse
 }
 
-function cp($path, $dest)
-{
+# Copy a file or directory to a new destination
+function cp($path, $dest) {
     Copy-Item $path $dest -Force -Recurse
 }
 
-function reboot()
-{
+# Reboot the computer
+function reboot() {
     Restart-Computer
 }
 
-function shutdown()
-{
+# Shutdown the computer
+function shutdown() {
     Stop-Computer
 }
 
-function head($path, $lines = 10)
-{
+# Display the first n lines of a file
+function head($path, $lines = 10) {
     Get-Content $path | Select-Object -First $lines
 }
 
-function x($path = ".")
-{
+# Open File Explorer at a specific path
+function x($path = ".") {
     explorer $path
 }
 
+# Functions for working with zip and unzip operations
 function unzip {
-    #    expand in smae directory
+    # Expand the archive in the same directory
     Expand-Archive -Path $args[0] -DestinationPath .
 }
 
 function zip {
-    # compress in same directory
+    # Compress the archive in the same directory
     Compress-Archive -Path $args[0] -DestinationPath "$($args[0]).zip"
 }
 
 function unzipr {
-    # expand in same directory and remove zip
+    # Expand the archive in the same directory and remove the original zip file
     Expand-Archive -Path $args[0] -DestinationPath .
     Remove-Item $args[0]
 }
 
 function zipr {
-    # compress in same directory and remove original
+    # Compress the archive in the same directory and remove the original files
     Compress-Archive -Path $args[0] -DestinationPath "$($args[0]).zip"
     Remove-Item $args[0]
 }
 
+# Function to get the public IP address using an external service
 function Get-PubIP {
-    (Invoke-WebRequest http://ifconfig.me/ip ).Content
+    (Invoke-WebRequest http://ifconfig.me/ip).Content
 }
 
-# go to a directory even in path is a file.
+# Function to navigate to a directory even if the path is a file
 function gotodir($dir) {
-	if ((get-item $dir -erroraction silentlycontinue ) -is [system.io.directoryinfo]) {
-		cd $dir
-	} else {
-		cd (split-path -path $dir)
-	}
+    if ((Get-Item $dir -ErrorAction SilentlyContinue) -is [System.IO.DirectoryInfo]) {
+        cd $dir
+    } else {
+        cd (Split-Path -Path $dir)
+    }
 }
 
-# find files by filename
+# Function to find files by filename
 function find-file($name) {
-        Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
-                $place_path = $_.directory
-                Write-Output "${place_path}\${_}"
-        }
+    Get-ChildItem -Recurse -Filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.Directory
+        Write-Output "${place_path}\${_}"
+    }
 }
 
-# ------------------------------Aliases------------------------------
+# Aliases for commonly used commands
 set-alias -name gd -value gotodir
 set-alias -name ff -value find-file
-
-# Set UNIX-like aliases for the admin command, so sudo <command> will 
-# run the command with elevated rights.
 Set-Alias -Name su -Value admin
 Set-Alias -Name sudo -Value admin
